c1e7d19d2b3db7a7f16462785bdb0b383b2a0d92
cyphercode005 <005.cypher@gmail.com>
2023-02-16 22:36:21 +0500

    Some Resp1

---
+++ src/components/Content.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Content() {
+  return (
+    <div className=' bg-black pt-10 lg:pt-16 xl:pt-16 pl-10 lg:pl-16 xl:pl-16'>
+        <div className=''>
+            <img className='h-24 lg:h-72 xl:h-72' src="/fig1.png" alt=''/>
+        </div>
+        <div className=' mt-7 text-white text-xl lg:text-4xl xl:text-4xl w-full lg:w-2/3 xl:w-2/3 pr-10 lg:pr-0 xl:pr-0'>
+        With volatility a constant threat to any crypto currency trader, we are offering the first ever coin which will allow you to hedge against against the unpredictable swings in the market.
+        </div>
+        <div className=' mt-16 lg:mt-28 xl:mt-28 flex flex-col lg:flex-row xl:flex-row pr-4 xl:pr-16 xl:pr-16 '>
+          <div className='lg:w-4/5 lg:w-4/5 text-white pl-6 lg:pl-0 xl:pl-0 border-l-2 lg:border-l-0 xl:border-l-0 lg:border-t-2 xl:border-t-2 text-lg pr-8'> <span className='pt-10'> 01. </span> <h3 className='pt-2'>Notice Trend  </h3> <p className='text-sm pt-2 text-justify'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,</p>  </div>
+          <div className='lg:w-4/5 lg:w-4/5 pl-6 lg:pl-0 xl:pl-0 border-l-2 lg:border-l-0 xl:border-l-0 mt-6 lg:mt-0 xl:mt-0 text-white lg:border-t-2 xl:border-t-2 text-lg pr-8'> <span> 02. </span> <h3 className='pt-2'> Purchase </h3> <p className='text-sm pt-2 text-justify'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,</p>  </div>
+          <div className='lg:w-4/5 lg:w-4/5 pl-6 lg:pl-0 xl:pl-0 border-l-2 lg:border-l-0 xl:border-l-0 mt-6 lg:mt-0 xl:mt-0 text-white lg:border-t-2 xl:border-t-2 text-lg pr-8'> <span> 03. </span> <h3 className='pt-2'> Hold  </h3> <p className='text-sm pt-2 text-justify'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,</p>  </div>
+          <div className='lg:w-4/5 lg:w-4/5 pl-6 lg:pl-0 xl:pl-0 border-l-2 lg:border-l-0 xl:border-l-0 mt-6 lg:mt-0 xl:mt-0 text-white lg:border-t-2 xl:border-t-2 text-lg pr-8'> <span> 04. </span> <h3 className='pt-2'> Sell </h3> <p className='text-sm pt-2 text-justify'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,</p>  </div>
+        </div>
+    </div>
+  )
+}
