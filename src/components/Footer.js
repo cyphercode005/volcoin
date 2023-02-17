65fe1323c0d454d07334236ebda26bcbd3b5f58d
cyphercode005 <005.cypher@gmail.com>
2023-02-17 16:16:46 +0500

    Complete Responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,30 +3,30 @@ import React from 'react'
 export default function Footer() {
   return (
 
-<footer className="pl-12 pr-12 mt-16 pb-16 p-4 shadow bg-black">
-    <div className="sm:flex sm:items-center sm:justify-between">
-        <a href="/" className="flex items-center mb-4 sm:mb-0">
-            <img src="logo.png" className="h-8 mr-3" alt="Flowbite Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">VOLCOIN</span>
+<footer className="pl-4 lg:pl-12 xl:pl-12 pr-4 lg:pr-12 xl:pr-12  bg-gray-900 mt-5 pt-4 lg:pt-6 xl:pt-6 lg:mt-16 xl:mt-16 pb-4 lg:pb-8 xl:pb-8 shadow bg-black">
+    <div className="flex items-center justify-between">
+        <a href="/" className="flex items-center">
+            <img src="logo.png" className="h-6 lg:h-8 xl:h-8 mr-3" alt="Flowbite Logo" />
+            <span className=" self-center text-lg lg:text-2xl xl:text-2xl font-semibold whitespace-nowrap dark:text-white">VOLCOIN</span>
         </a>
-        <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+        <ul className="flex flex-wrap items-center text-xs xl:text-sm lg:text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
-                <a href="/" className="mr-4 hover:underline md:mr-6 ">WHITEPAPER</a>
+                <a href="/" className=" hidden lg:block xl:block mr-2 lg:mr-6 xl:mr-6 hover:underline ">WHITEPAPER</a>
             </li>
             <li>
-                <a href="/" className="mr-4 hover:underline md:mr-6">EMAIL US</a>
+                <a href="/" className="mr-2 lg:mr-6 xl:mr-6 hover:underline">EMAIL US</a>
             </li>
             <li>
-                <a href="/" className="mr-4 hover:underline md:mr-6 ">TWITTER</a>
+                <a href="/" className="hidden lg:block xl:block mr-2 lg:mr-6 xl:mr-6 hover:underline">TWITTER</a>
             </li>
             <li>
-                <a href="/" className="mr-4 hover:underline">PRIVACY</a>
+                <a href="/" className="mr-2 lg:mr-6 xl:mr-6 hover:underline">PRIVACY</a>
             </li>
             <li>
-                <a href="/" className="mr-4 hover:underline">T&C</a>
+                <a href="/" className="mr-2 lg:mr-6 xl:mr-6 hover:underline">T&C</a>
             </li>
             <li>
-            <button className='border border-gray-400 rounded mx-0 lg:mx-2 xl:mx-2 py-1 px-1 lg:px-2 xl:px-3 text-xs text-gray-400'> PURCHASE VOLCOIN </button>
+            <button className='hidden lg:block xl:block border border-gray-400 rounded mx-0 lg:mx-2 xl:mx-2 py-1 px-1 lg:px-2 xl:px-3 text-xs text-gray-400'> PURCHASE VOLCOIN </button>
             </li>
         </ul>
     </div>
--- src/components/RoadMap.js
+++ src/components/RoadMap.js
@@ -2,65 +2,65 @@ import React from "react";
 
 export default function RoadMap() {
   return (
-    <div className="pl-4 lg:pl-16 xl:pl-16 text-white flex justify-between">
-      <div className=" mt-16 lg:mt-36 xl:mt-36 lg:w-4/5 xl:w-4/5">
+    <div className="w-screen pl-4 lg:pl-16 xl:pl-16 text-white flex justify-between">
+      <div className="absolute lg:block xl:block mt-16 lg:mt-36 xl:mt-36 lg:w-4/5 xl:w-4/5">
         <span className=" text-4xl lg:text-9xl xl:text-9xl"> Our Roadmap </span>
-        <p className=" pl-3 mt-6 lg:mt-12 xl:mt-12 text-md lg:w-3/5 xl:w-3/5">
+        <p className="mr-12 lg:mr-0 xl:mr-0 pl-3 mt-6 lg:mt-12 xl:mt-12 text-md lg:w-3/5 xl:w-3/5">
           With volatility a constant threat to any crypto currency trader, we
           are offering the first ever coin which will allow you to hedge against
           the unpredictable swings in the market.
         </p>
-        <div className="flex flex-col lg:flex-row xl:flex-row" >
-        <section className="pl-5 lg:pl-12 xl:pl-12 pb-10 mt-16 lg:w-3/5 xl:w-3/5 ml-5 lg:ml-3 xl:ml-3 border border-gray-300">
-          <img className="mt-12 mb-12 w-16" src="roadmap1.png" alt="" />
+        <div className="flex flex-col-reverse lg:flex-row xl:flex-row" >
+        <section className="mr-20 lg:mr-0 xl:mr-0 pl-5 lg:pl-12 xl:pl-12 pb-8 mt-8 lg:mt-16 xl:mt-16 lg:w-3/5 xl:w-3/5 ml-5 lg:ml-3 xl:ml-3 border border-gray-300">
+          <img className="mt-8 lg:mt-12 xl:mt-12 w-12 pb-4 lg:pb-8 xl:pb-8" src="roadmap1.png" alt="" />
           <span className="text-sm text-yellow-300"> Phase Three</span>
-          <h1 className=" pt-3 text-3xl w-1/2">
+          <h1 className=" pt-3 text-xl lg:text-3xl xl:text-3xl lg:w-1/2 xl:w-1/2">
             Create GVolCoins and Marketing
           </h1>
-          <section className="mt-7">
-              <div className="flex pr-16">
+          <section className="mt-4 lg:mt-7 xl:mt-7">
+              <div className="flex text-justify pr-4 lg:pr-16 xl:pr-16">
                 <img className="w-3 h-3 mt-1 mr-2" src="tick.png" alt="" />
-                <p className="text-sm">
+                <p className="text-xs lg:text-sm xl:text-sm">
                   The governance tokens will be used to attract initial
                   investment from investors looking to enter into the project
                   early at a discount, whitelist distribution.
                 </p>
             </div>
-              <div className="flex mt-3 pr-16">
+              <div className="flex mt-3 text-justify pr-4 lg:pr-16 xl:pr-16">
                 <img className="w-3 h-3 mt-1 mr-2" src="tick.png" alt="" />
-                <p className="text-sm">
+                <p className="text-xs lg:text-sm xl:text-sm">
                 Start marketing organically using Twitter, Discord and Telegram
                 </p>
             </div>
-              <div className="flex mt-3 pr-16">
+              <div className="flex mt-3 text-justify pr-4 lg:pr-16 xl:pr-16">
                 <img className="w-3 h-3 mt-1 mr-2" src="tick.png" alt="" />
-                <p className="text-sm">
+                <p className="text-xs lg:text-sm xl:text-sm">
                 Work on giveaways to attract early adopters
                 </p>
             </div>
-              <div className="flex mt-3 pr-16">
+              <div className="flex mt-3 text-justify pr-4 lg:pr-16 xl:pr-16">
                 <img className="w-3 h-3 mt-1 mr-2" src="tick.png" alt="" />
-                <p className="text-sm">
+                <p className="text-xs lg:text-sm xl:text-sm">
                 Legal preparation for regulation compliance
                 </p>
             </div>
           </section>
         </section>
-        <ul className="pl-20 mt-20 ml-3 flex flex-col list-disc">
-            <li className=" text-gray-300"> Phase One. </li>
-            <li  className="pt-7 text-gray-300"> Phase Two.</li>
-            <li  className="pt-7 text-yellow-300"> Phase Three.</li>
-            <li  className="pt-7 text-gray-500"> Phase Four.</li>
-            <li  className="pt-7 text-gray-500"> Phase Five.</li>
-            <li  className="pt-7 text-gray-500"> Phase Six.</li>
-            <li  className="pt-7 text-gray-500"> Phase Seven.</li>
-            <li  className="pt-7 text-gray-500"> Phase Eight.</li>
-            <li  className="pt-7 text-gray-500"> Phase Nine.</li>
+        <ul className="mt-10 lg:mt-20 xl:mt-20 pl-8 lg:pl-20 xl:pl-20 ml-3 lg:ml-3 xl:ml-3 flex lg:flex-col xl:flex-col list-disc gap-0	">
+            <li className=" text-gray-300 px-3"> <span className="hidden lg:block xl:block"> Phase One.</span> </li>
+            <li  className="lg:pt-7 xl:pt-7 text-gray-300 px-3"> <span className="hidden lg:block xl:block"> Phase Two.</span></li>
+            <li  className="lg:pt-7 xl:pt-7 text-yellow-300 px-3"><span className="hidden lg:block xl:block"> Phase Three.</span> </li>
+            <li  className="lg:pt-7 xl:pt-7 text-gray-500 px-3"> <span className="hidden lg:block xl:block"> Phase Four.</span></li>
+            <li  className="lg:pt-7 xl:pt-7 text-gray-500 px-3"><span className="hidden lg:block xl:block"> Phase Five.</span> </li>
+            <li  className="lg:pt-7 xl:pt-7 text-gray-500 px-3"> <span className="hidden lg:block xl:block"> Phase Six.</span> </li>
+            <li  className="lg:pt-7 xl:pt-7 text-gray-500 px-3"> <span className="hidden lg:block xl:block"> Phase Seven.</span> </li>
+            <li  className="lg:pt-7 xl:pt-7 text-gray-500 px-3"> <span className="hidden lg:block xl:block"> Phase Eight.</span> </li>
+            <li  className="lg:pt-7 xl:pt-7 text-gray-500 px-3"> <span className="hidden lg:block xl:block">  Phase Nine.</span> </li>
         </ul>
         </div>
       </div>
-      <div className="flex justify-end">
-        <div className="flex justify-end"> <img className="" src="roadmap2.png" alt="" /> <div> </div></div>
+      <div className="pl-16 lg:pl-9 xl:pl-9 lg:left-2/3 xl:left-2/3 relative lg:block xl:block justify-end">
+        <div className="flex justify-end"> <img className="w-72 lg:w-96 xl:w-96" src="roadmap2.png" alt="" /> <div> </div></div>
       </div>
     </div>
   );
