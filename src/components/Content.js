bfd3050766226340c1f8c958df5af506b232168d
cyphercode005 <005.cypher@gmail.com>
2023-03-18 12:04:59 +0500

    Changes for XXL

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,13 +4,13 @@ export default function Content() {
   return (
     <div className='pt-10 lg:pt-16 xl:pt-0 2xl:pt-0 xxl:pt-0 pl-4 lg:pl-16 xl:pl-16 2xl:pl-16 xxl:pl-16'>
       <div className='justify-between flex'>
-        <div className='absolute xl:static w-full lg:w-2/3 xl:w-2/3 2xl:w-2/3 xxl:w-2/3 pt-4 lg:pt-20 xl:pt-20 2xl:pt-20 xxl:pt-20'> 
-         <img className='h-24 lg:h-28 xl:h-32 2xl:h-36 xxl:h-40' src="/Photos/content1.png" alt='' /> 
-        <div className=' pt-4 lg:pt10 xl:pt-12 2xl:pt-16 xxl:pt-20 text-white text-xl lg:text-4xl xl:text-4xl 2xl:text-5xl xxl:text-6xl  w-full pr-0 lg:pr-0 xl:pr-0 2xl:pr-0 xxl:pr-0'>
-        With volatility constant threat to any crypto currency trader, we are offering the first ever coin which will allow you to hedge against against the unpredictable swings in the market.
+        <div className='w-full lg:w-2/3 xl:w-2/3 2xl:w-2/3 xxl:w-2/3 pt-4 lg:pt-20 xl:pt-20 2xl:pt-20 xxl:pt-20'> 
+         <img className='h-20 lg:h-28 xl:h-32 2xl:h-36 xxl:h-40' src="/Photos/content1.png" alt='' /> 
+        <div className=' pt-4 lg:pt10 xl:pt-12 2xl:pt-16 xxl:pt-20 text-white text-xl lg:text-4xl xl:text-4xl 2xl:text-5xl xxl:text-6xl w-full'>
+        With volatility a constant threat to any crypto currency trader, we are offering the first ever coin which will allow you to hedge against against the unpredictable swings in the market.
       </div>
         </div>
-        <div className='relative xl:static pt-0 lg:pt-0 xl:pt-0 2xl:pt-0 xxl:pt-0'> <img className='h-72 lg:h-96 xl:h-96 2xl:h-96 xxl:h-auto' src="Photos/content2.png" alt="" /> </div>
+        <div className='pt-0 lg:pt-0 xl:pt-0 2xl:pt-0 xxl:pt-0'> <img className='h-72 lg:h-96 xl:h-96 2xl:h-96 xxl:h-auto' src="Photos/content2.png" alt="" /> </div>
       </div>
       <div className=' mt-16 lg:mt-28 xl:mt-28 flex flex-col lg:flex-row xl:flex-row pr-4 lg:pr-16 xl:pr-16 '>
         <div className=' text-white pl-6 lg:pl-0 xl:pl-0 2xl:pl-0 xxl:pl-0 lg:pr-10 xl:pr-12 2xl:pr-16 xxl:pr-20 border-l-2 lg:border-l-0 xl:border-l-0 lg:border-t-2 xl:border-t-2 text-lg pr-8 lg:pt-5 xl:pt-6 2xl:pt-7 xxl:pt-8'> <span className='pt-10 2xl:text-xl xxl:text-2xl'> 01. </span> <h3 className='pt-2 2xl:text-3xl xxl:text-4xl'>Notice Trend  </h3> <p className='text-sm pt-2 2xl:text-xl xxl:text-xl'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,</p>  </div>
